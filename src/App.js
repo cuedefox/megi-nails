8fbeecea10417655b75b6b5db0e3122a7733c5f6
rodrigo vergara <rodrigovergara2001@gmail.com>
2024-10-14 17:54:56 -0300

    fix NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Footer } from "./containers/Footer";
 import { MainContainer } from "./containers/MainContainer";
-import { NavBar } from "./containers/Navbar"; 
+import { NavBar } from "./containers/NavBar"; 
 import "../src/styles/styles.scss";
 
 function App() {
