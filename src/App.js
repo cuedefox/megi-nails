a66142ea1083d90340eaaabd95a8cb1fa8389b7e
rodrigo vergara <rodrigovergara2001@gmail.com>
2024-10-14 17:50:04 -0300

    cache clean

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Footer } from "./containers/Footer";
 import { MainContainer } from "./containers/MainContainer";
-import { NavBar } from "./containers/NavBar";
 import "../src/styles/styles.scss";
 
 function App() {
   return <>
-      <NavBar />
       <MainContainer />
       <Footer />
     </>
