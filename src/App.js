dc821a5ed390568637bbd2b1765df0f47d856119
rodrigo vergara <rodrigovergara2001@gmail.com>
2024-10-14 17:51:09 -0300

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Footer } from "./containers/Footer";
 import { MainContainer } from "./containers/MainContainer";
+import { NavBar } from "./containers/Navbar"; 
 import "../src/styles/styles.scss";
 
 function App() {
   return <>
+      <NavBar />
       <MainContainer />
       <Footer />
     </>
