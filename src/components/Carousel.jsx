a7be22d830c0b4f5c589734dbaef1bcb8bcef5cc
rodrigo vergara <rodrigovergara2001@gmail.com>
2024-10-14 22:37:07 -0300

    update carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,17 +3,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export const Carousel = () => {
-    return <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    return <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
     <div class="carousel-inner">
-      <div class="carousel-item active" data-bs-interval="3600">
-        <img src="https://cloudfront-us-east-1.images.arcpublishing.com/semana/X2YPEFWMAREMBF5W72PPIHFU54.jpg" class="d-block w-100" alt="..." />
+      <div class="carousel-item active">
+        <img src="..." class="d-block w-100" alt="..." />
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
       </div>
-      <div class="carousel-item" data-bs-interval="3600">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJN-vU3YCrG0yubfohitCanF8MsQ4R3Ly-nQ&s" class="d-block w-100" alt="..." />
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="..." />
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+        </div>
       </div>
-      <div class="carousel-item" data-bs-interval="3600">
-        <img src="https://cloudfront-us-east-1.images.arcpublishing.com/semana/X2YPEFWMAREMBF5W72PPIHFU54.jpg" class="d-block w-100" alt="..." />
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="..." />
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
       </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
 }
