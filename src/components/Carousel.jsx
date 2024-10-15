91f7554289b6e85e5e674d28ce38bb6264800b42
rodrigo vergara <rodrigovergara2001@gmail.com>
2024-10-14 22:17:14 -0300

    fix nav

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,30 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export const Carousel = () => {
-    return <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
+    return <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="d-block w-100" src="https://cloudfront-us-east-1.images.arcpublishing.com/semana/X2YPEFWMAREMBF5W72PPIHFU54.jpg" alt="First slide" />
+      <div class="carousel-item active" data-bs-interval="3600">
+        <img src="https://cloudfront-us-east-1.images.arcpublishing.com/semana/X2YPEFWMAREMBF5W72PPIHFU54.jpg" class="d-block w-100" alt="..." />
       </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJN-vU3YCrG0yubfohitCanF8MsQ4R3Ly-nQ&s" alt="Second slide" />
+      <div class="carousel-item" data-bs-interval="3600">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJN-vU3YCrG0yubfohitCanF8MsQ4R3Ly-nQ&s" class="d-block w-100" alt="..." />
       </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="https://cloudfront-us-east-1.images.arcpublishing.com/semana/X2YPEFWMAREMBF5W72PPIHFU54.jpg" alt="Third slide" />
+      <div class="carousel-item" data-bs-interval="3600">
+        <img src="https://cloudfront-us-east-1.images.arcpublishing.com/semana/X2YPEFWMAREMBF5W72PPIHFU54.jpg" class="d-block w-100" alt="..." />
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
   </div>
 }
